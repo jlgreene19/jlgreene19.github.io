0c64bf33a01de2e1094a5addbfe31f42ed3ae57d
jlgreene19 <jlgreene19@byui.edu>
2020-10-29 14:03:26 -0700

    Added script src web font for lazyload

--- WDD 230/lesson7/js/imagehandling.js
+++ WDD 230/lesson7/js/imagehandling.js
@@ -2,7 +2,7 @@ let imagesToLoad = document.querySelectorAll('img[data-src]');
 
 const imgOptions = {
     threshold: 1,
-    rootMargin: "0px 0px 200px 0px"
+    rootMargin: "0px 0px 250px 0px"
 };
 
 const loadImages = (image) => {
