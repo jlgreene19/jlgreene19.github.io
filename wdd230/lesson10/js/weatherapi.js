f39b02c3169db9d6e3d42d6bf9d215b2516e108f
jlgreene19 <jlgreene19@byui.edu>
2020-11-17 17:02:24 -0800

    Updated apiURL address

--- wdd230/lesson10/js/weatherapi.js
+++ wdd230/lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = "api.openweathermap.org/data/2.5/weather?id=5604473&appid=014fa4af92532fb406ce8595701f9194";
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=014fa4af92532fb406ce8595701f9194';
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
