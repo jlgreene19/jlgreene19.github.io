5ae15b3376126db54c3cc9de186370d1d170321a
jlgreene19 <jlgreene19@byui.edu>
2020-11-18 09:53:14 -0800

    Updates to Weather Page

---
+++ wdd230/lesson10/js/windchill.js
@@ -0,0 +1,22 @@
+function output() {
+    let temp = parseFloat(document.getElementById("temp").textContent);
+    let wSpeed = parseFloat(document.getElementById("wSpeed").textContent);
+    let output = windChill(temp, wSpeed);
+
+    document.getElementById("windChill").textContent = output.toFixed(0);
+}
+
+function windChill(temp, wSpeed) {
+    if (temp <= 50 && wSpeed > 3) {
+        let te = temp;
+        let ws = wSpeed;
+        let windChill = 35.74 + (0.6215 * te) - (35.75 * Math.pow(ws, 0.16)) + (0.4275 * te * Math.pow(ws, 0.16));
+
+        return windChill;
+    }
+    else {
+        if (temp > 50 || wSpeed <= 3)
+        document.getElementById("windChill").textContent = "N/A";
+    }
+}
+output()
