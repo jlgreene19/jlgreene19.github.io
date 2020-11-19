fed4c8194790997c056b8332e1c9b16c027dbbd6
jlgreene19 <jlgreene19@byui.edu>
2020-11-19 11:59:08 -0800

    Fixed Weather API windchill functionality

--- wdd230/lesson10/js/weatherinfo.js
+++ wdd230/lesson10/js/weatherinfo.js
@@ -7,4 +7,29 @@ fetch(apiURL)
     document.getElementById('temp').textContent = jsObject.main.temp;
     document.getElementById('humidity').textContent = jsObject.main.humidity;
     document.getElementById('wSpeed').textContent = jsObject.wind.speed;
-  });
+  
+
+  function output() {
+    let temp = parseFloat(document.getElementById("temp").textContent);
+    let wSpeed = parseFloat(document.getElementById("wSpeed").textContent);
+    let output = windChill(temp, wSpeed);
+
+    document.getElementById("windChill").textContent = output.toFixed(0);
+}
+
+function windChill(temp, wSpeed) {
+    if (temp <= 50 && wSpeed > 3) {
+        let te = temp;
+        let ws = wSpeed;
+        let windChill = 35.74 + (0.6215 * te) - (35.75 * Math.pow(ws, 0.16)) + (0.4275 * te * Math.pow(ws, 0.16));
+
+        return windChill;
+    }
+    else {
+        
+        document.getElementById("windChill").textContent = "N/A";
+    }
+}
+output()
+
+});
