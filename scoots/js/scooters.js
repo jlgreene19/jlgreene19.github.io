a9bf4824ef7baedd0ea5456f8fd40c50b9d435a1
jlgreene19 <jlgreene19@byui.edu>
2020-12-11 15:54:28 -0800

    test push

--- scoots/js/scooters.js
+++ scoots/js/scooters.js
@@ -1,6 +1,6 @@
-const requestURL = 'data/scooters.json';
+const requestURLscoot = 'data/scooters.json';
 
-fetch(requestURL)
+fetch(requestURLscoot)
   .then(function (response) {
     return response.json();
   })
@@ -8,28 +8,28 @@ fetch(requestURL)
     console.table(jsonObject);  // temporary checking for valid response and data parsing
     const scooters = jsonObject['scooters'];
     for (let i = 0; i < scooters.length; i++ ) {
-      if (scooters[i].name == "Preston" || scooters[i].name == "Soda Springs" || scooters[i].name == "Fish Haven"){
+      if (scooters[i].name == "~Honda Metro Scooter" ||scooters[i].name == "~Honda Dio Scooter" || towns[i].name == "Fish Haven"){
 
         let card = document.createElement('section');
         let info = document.createElement('div');
         let h2 = document.createElement('h2');
-        let motto = document.createElement('p');
-        let yearFounded = document.createElement('p');
-        let currentPopulation = document.createElement('p');
-        let averageRainfall = document.createElement('p');
+        let e1 = document.createElement('p');
+        let e2 = document.createElement('p');
+        let e3 = document.createElement('p');
+        let e4 = document.createElement('p');
         let image = document.createElement('img');
 
         h2.textContent = scooters[i].name
-        motto.textContent = ' ' + scooters[i].motto;
-        yearFounded.textContent = 'Year Founded: ' + scooters[i].yearFounded;
-        currentPopulation.textContent = 'Population: ' + scooters[i].currentPopulation;
-        averageRainfall.textContent = 'Annual Rain Fall: ' + scooters[i].averageRainfall;
+        e1.textContent = scooters[i].events[0];
+        e2.textContent = scooters[i].events[1];
+        e3.textContent = scooters[i].events[2];
+        e4.textContent = scooters[i].events[3];
 
         info.appendChild(h2);
-        info.appendChild(motto);
-        info.appendChild(yearFounded);
-        info.appendChild(currentPopulation);
-        info.appendChild(averageRainfall);
+        info.appendChild(e1);
+        info.appendChild(e2);
+        info.appendChild(e3);
+        info.appendChild(e4);
 
         card.appendChild(info);
         /* card.appendChild(h2);
@@ -40,7 +40,7 @@ fetch(requestURL)
         card.appendChild(image); */
         card.appendChild(image);
 
-        document.querySelector('div.cards').appendChild(card);
+        document.querySelector('div.scooters').appendChild(card);
         image.setAttribute('src', 'images/' + scooters[i].photo);
         image.setAttribute('alt', scooters[i].name + ' with photo');
     }}
